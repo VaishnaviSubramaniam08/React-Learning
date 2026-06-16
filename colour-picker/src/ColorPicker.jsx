fef5eb20282379c579225010ea3a67edd3abc4e8
Vaishnavi S <vaishnavisubramaniam247@gmail.com>
2026-06-16 15:47:16 +0530

    modified styles

--- colour-picker/src/ColorPicker.jsx
+++ colour-picker/src/ColorPicker.jsx
@@ -3,11 +3,9 @@ import "./index.css";
 function ColorPicker(){
     const [color,setColor]=useState("#ffffff");
     return (
-        <div  style={{
-  backgroundColor: color
-}}>
+        <div  style={{backgroundColor: color }}>
             <h1>Color Picker</h1>
-            <input type="color" value={color} onChange={(e)=> setColor(e.target.value)}/>
+            <input type="color" value={color} onChange={(e)=> setColor(e.target.value)} />
             <h2>Selected Color :{ color} </h2>
         </div>
     );
